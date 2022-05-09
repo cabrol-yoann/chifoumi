c6c3cb5c1e248aa90a650325255c0bf3744228be
yoann cabrol <52161115+yoanyoyo@users.noreply.github.com>
2022-05-09 15:29:44 +0200

    V1
    version 1 du projet

--- chifoumi.cpp
+++ chifoumi.cpp
@@ -1,46 +1,85 @@
 #include "chifoumi.h"
-
+#include "ui_chifoumi.h"
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <QPixmap>
+#include <QDebug>
+#include <QString>
+#include <QMessageBox>
 
 
-    ///* ---- PARTIE MOD?LE ---------------------------
-
-Chifoumi::Chifoumi():
-
-
-    //ctor
-    // partie mod?le
-     scoreJoueur(0),
-     scoreMachine(0),
-     coupJoueur(rien),
-     coupMachine(rien)
-{}
-
-Chifoumi::~Chifoumi()
+        ///Définition constructeur et destructeur
+chifoumi::chifoumi(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::chifoumi)
 {
-    //dtor
+    ui->setupUi(this);
+    connect(ui -> bNPartie, SIGNAL(clicked()),this, SLOT(lancerPartie()));
+    connect(ui -> bCiseau, SIGNAL(clicked()),this, SLOT(choixCiseau()));
+    connect(ui -> bPierre, SIGNAL(clicked()),this, SLOT(choixPierre()));
+    connect(ui -> bPapier, SIGNAL(clicked()),this, SLOT(choixPapier()));
+    connect(ui -> actionAide, SIGNAL(triggered()),this, SLOT(infoTexte()));
 }
 
-        /// Getters
-
-Chifoumi::UnCoup Chifoumi::getCoupJoueur() {
-    return coupJoueur;
+chifoumi::~chifoumi()
+{
+    delete ui;
 }
 
-Chifoumi::UnCoup Chifoumi::getCoupMachine() {
-    return coupMachine;
+
+    ///Définition méthode
+void chifoumi::modificationLogo(UnCoup coupMachine, UnCoup coupJoueur)
+{
+    //initialisation des logos
+    QPixmap logoCiseau;
+    logoCiseau.load(":/Chifoumi/ciseau_115.png");
+    QPixmap logoPierre;
+    logoPierre.load(":/Chifoumi/pierre_115.png");
+    QPixmap logoPapier;
+    logoPapier.load(":/Chifoumi/papier_115.png");
+
+    // modification du logo en fonction du coup généré
+    switch (coupMachine)
+    {
+        case ciseau :
+        ui -> coupMachine -> setPixmap(logoCiseau);
+        break;
+
+        case pierre :
+        ui -> coupMachine -> setPixmap(logoPierre);
+        break;
+
+        case papier :
+        ui -> coupMachine -> setPixmap(logoPapier);
+        break;
+
+        case rien :
+        break;
+    }
+
+    // modification du logo du joueur en fonction de son coup
+    switch (coupJoueur)
+    {
+        case ciseau :
+        ui -> coupJoueur -> setPixmap(logoCiseau);
+        break;
+
+        case pierre :
+        ui -> coupJoueur -> setPixmap(logoPierre);
+        break;
+
+        case papier :
+        ui -> coupJoueur -> setPixmap(logoPapier);
+        break;
+
+        case rien :
+        break;
+
+    }
 }
 
-unsigned int Chifoumi::getScoreJoueur() {
-    return scoreJoueur;
-}
-
-unsigned int Chifoumi::getScoreMachine() {
-    return scoreMachine;
-}
-
-char Chifoumi::determinerGagnant()
+char chifoumi::determinerGagnant(UnCoup coupMachine, UnCoup coupJoueur)
 {
     char gagnantARetourner;
 
@@ -48,91 +87,47 @@ char Chifoumi::determinerGagnant()
     gagnantARetourner = 'N';
 
     // il sera modifi? dans l'un des cas suivants
-    if(getCoupJoueur() < getCoupMachine())
-    {gagnantARetourner='M';}
+    if(coupJoueur == pierre && coupMachine == ciseau){
+        gagnantARetourner= 'J';
+    }
 
-    else if (getCoupJoueur() == ciseau && getCoupMachine() == pierre){
+    else if (coupJoueur == ciseau && coupMachine == pierre){
         gagnantARetourner= 'M';
     }
 
 
-    else if(getCoupJoueur() > getCoupMachine())
-    {gagnantARetourner='J';}
+    else if(coupJoueur > coupMachine){
+        gagnantARetourner='J';
+    }
 
-    else if (getCoupJoueur() == pierre && getCoupMachine() == ciseau){
-        gagnantARetourner= 'J';
+    else if (coupJoueur < coupMachine){
+        gagnantARetourner='M';
     }
 
     return gagnantARetourner;
 }
 
-         ///* M?thodes utilitaires du Mod?le
-
-int randMinMax(int min, int max){
-    /* pr?-condition : min<max ;
-       Le nbre al?atoire est compris entre [min, max[ */
-   return rand()%(max-min) + min;
-}
-
-Chifoumi::UnCoup Chifoumi::genererUnCoup()
+void chifoumi::majScores(char p_gagnant)
 {
-    UnCoup valeurGeneree;   // valeur à retourner
-    unsigned int nbAleatoire;
-    nbAleatoire = randMinMax(1,4);
-    switch (nbAleatoire)
-    {
-    case 1 :
-        valeurGeneree = pierre;
-        break;
-    case 2 :
-        valeurGeneree = papier;
-        break;
-    case 3 :
-        valeurGeneree = ciseau;
-        break;
-    default:
-        break;
-    }
+    // Variable pour convertir la valeur et la modifier
+    bool ok = true;
+    int val = 0;
+    QString txt = " ";
 
-    return valeurGeneree;
-}
-
-        /// Setters
-
-void Chifoumi::setCoupJoueur(UnCoup p_coup) {
-    coupJoueur = p_coup;
-}
-
-void Chifoumi::setCoupMachine(UnCoup p_coup) {
-    coupMachine = p_coup;
-}
-
-void Chifoumi::setScoreJoueur(unsigned int p_score) {
-    scoreJoueur = p_score;
-}
-
-void Chifoumi::setScoreMachine(unsigned int p_score) {
-    scoreMachine = p_score;
-}
-
-void Chifoumi::majScores(char p_gagnant) {
-
-    if(determinerGagnant() == 'M')
-    {p_gagnant = 'M';}
-
-    else if(determinerGagnant() == 'J')
-    {p_gagnant = 'J';}
-
-    else if(determinerGagnant() == 'N')
-    {p_gagnant = 'N';}
-
+    // modification des score incrémentation en cas de victoire sinon aucune modification
     switch (p_gagnant)
     {
     case 'M' :
-        setScoreMachine(getScoreMachine()+1);
+        val = ui -> scoreMachine -> text().toInt(&ok);
+        val = val +1;
+        txt = QString::number(val);
+        ui -> scoreMachine -> setText(txt);
         break;
     case 'J':
-        setScoreJoueur(getScoreJoueur()+1);
+        val = ui -> scoreJoueur -> text().toInt(&ok);
+        val = val +1;
+        txt = QString::number(val);
+        ui -> scoreJoueur -> setText(txt);
         break;
     case 'N':
         break;
@@ -142,12 +137,93 @@ void Chifoumi::majScores(char p_gagnant) {
     return;
 }
 
-void Chifoumi::initScores() {
-    scoreJoueur = 0;
-    scoreMachine = 0;
+chifoumi::UnCoup chifoumi::genererUnCoup()
+{
+    UnCoup valeurGeneree;   // valeur à retourner
+    unsigned int nbAleatoire;
+    nbAleatoire = rand()%(4-1) + 1;
+    switch (nbAleatoire)
+    {
+    case 1 :
+        valeurGeneree = ciseau;
+        break;
+    case 2 :
+        valeurGeneree = pierre;
+        break;
+    case 3 :
+        valeurGeneree = papier;
+        break;
+    }
+    return valeurGeneree;
 }
 
-void Chifoumi::initCoups() {
-    setCoupJoueur(rien);
-    setCoupMachine(rien);
+    ///SLOTS
+void chifoumi::lancerPartie()
+{
+
+    ui -> scoreJoueur -> setStyleSheet("color : blue");
+    ui -> LJoueur -> setStyleSheet("color : blue");
+
+    // initialisation des boutons
+    ui -> bCiseau -> setEnabled(true);
+    ui -> bPierre -> setEnabled(true);
+    ui -> bPapier -> setEnabled(true);
+
+    // initialisation des scores
+    ui -> scoreJoueur -> setText("0");
+    ui -> scoreMachine -> setText("0");
+
+    //initialisation des logos
+    QPixmap logoRien;
+    logoRien.load(":/Chifoumi/rien_115.png");
+    ui -> coupJoueur -> setPixmap(logoRien);
+    ui -> coupMachine -> setPixmap(logoRien);
 }
+
+void chifoumi::choixCiseau()
+{
+    // modification des logos (joueur/machine)
+    UnCoup coupJoueur;
+    coupJoueur = ciseau;
+    UnCoup coupMachine;
+    coupMachine = genererUnCoup();
+    modificationLogo(coupMachine,coupJoueur);
+
+    // modification du score
+    majScores(determinerGagnant(coupMachine, coupJoueur));
+}
+
+void chifoumi::choixPierre()
+{
+    // modification des logos (joueur/machine)
+    UnCoup coupJoueur;
+    coupJoueur = pierre;
+    UnCoup coupMachine;
+    coupMachine = genererUnCoup();
+    modificationLogo(coupMachine,coupJoueur);
+
+    // modification du score
+    majScores(determinerGagnant(coupMachine, coupJoueur));
+}
+
+void chifoumi::choixPapier()
+{
+    // modification des logos (joueur/machine)
+    UnCoup coupJoueur;
+    coupJoueur = papier;
+    UnCoup coupMachine;
+    coupMachine = genererUnCoup();
+    modificationLogo(coupMachine,coupJoueur);
+
+    // modification du score
+    majScores(determinerGagnant(coupMachine, coupJoueur));
+}
+
+void chifoumi::infoTexte()
+{
+    QMessageBox maBox;
+    maBox.setText("version: 1. \n Date de creation:04/05/2022. \n Créer par Cabrol Yoann, Da Silva Eduardo et Lauriou Tanguy. ");
+    maBox.exec();
+}
+
+
--- chifoumi.h
+++ chifoumi.h
@@ -1,82 +1,49 @@
-/***************************************************************
- * Name:      chifoumiMain.h
- * Author:    P.Dagorret ()
- * Created:   2021-05-10
- * Description : classe métier (= Modèle) Chifoumi-v1
- **************************************************************/
 #ifndef CHIFOUMI_H
 #define CHIFOUMI_H
-#include <iostream>
 
-using namespace std;
+#include <QMainWindow>
 
-class Chifoumi
+QT_BEGIN_NAMESPACE
+namespace Ui { class chifoumi; }
+QT_END_NAMESPACE
+
+class chifoumi : public QMainWindow
 {
-    ///* ---- PARTIE MODèLE ---------------------------
+    Q_OBJECT
 
-        ///* Une définition de type énuméré
-    public:
-        enum UnCoup {pierre, papier, ciseau, rien};
+    ///* Une dï¿½finition de type ï¿½numï¿½rï¿½
+public:
+    enum UnCoup {pierre, papier, ciseau, rien};
 
-        ///* Méthodes du Modèle
-    public:
-        Chifoumi();
-        virtual ~Chifoumi();
+    /// DÃ©finition constructeur et destructeur
+public:
+    chifoumi(QWidget *parent = nullptr);
+    ~chifoumi();
 
-        // Getters
-        UnCoup getCoupJoueur();
-            /* retourne le dernier coup joué par le joueur */
-        UnCoup getCoupMachine();
-            /* retourne le dernier coup joué par le joueur */
-        unsigned int getScoreJoueur();
-            /* retourne le score du joueur */
-        unsigned int getScoreMachine();
-            /* retourne le score de la machine */
-        char determinerGagnant();
-            /* détermine le gagnant 'J' pour joueur, 'M' pour machine, 'N' pour match nul
-               en fonction du dernier coup joué par chacun d'eux */
+    /// DÃ©finition mÃ©thode
+private :
+    UnCoup genererUnCoup();
+/* retourne une valeur alï¿½atoire = pierre, papier ou ciseau.
+   Utilisï¿½e pour faire jouer la machine */
+public:
+    void modificationLogo(UnCoup, UnCoup);
+    char determinerGagnant(UnCoup,UnCoup);
+/* Retourne un caractÃ¨re en fonction du gagnant de la partie.
+   Soit J pour le joueur, M pour la machine et N en cas de match Null */
+    void majScores(char p_gagnant);
+/* mise Ã  jour des score en fonction de determinerGagnant */
 
-         ///* Méthodes utilitaires du Modèle
-    private :
-        UnCoup genererUnCoup();
-    /* retourne une valeur aléatoire = pierre, papier ou ciseau.
-       Utilisée pour faire jouer la machine */
+    /// DÃ©finition SLOTS
+private slots:
+    void lancerPartie();
+/* Slot qui initialise la partie score Ã  0, des logo vide et activation des bouton */
+    void choixCiseau();
+    void choixPierre();
+    void choixPapier();
+/*Slots qui execute la manche en fonction du choix du joueur */
+    void infoTexte();
 
-        // Setters
-    public:
-        void setCoupJoueur(UnCoup p_coup);
-            /* initialise l'attribut coupJoueur avec la valeur
-               du paramètre p_coup */
-        void setCoupMachine(UnCoup p_coup);
-             /* initialise l'attribut coupmachine avec la valeur
-               du paramètre p_coup */
-        void setScoreJoueur(unsigned int p_score);
-            /* initialise l'attribut scoreJoueur avec la valeur
-               du paramètre p_score */
-        void setScoreMachine(unsigned int p_score);
-             /* initialise l'attribut coupMachine avec la valeur
-               du paramètre p_score */
-
-        // Autres modificateurs
-         void majScores(char p_gagnant);
-			/* Mise à jour des scores en fonction des règles de gestion actuelles :
-				- 1 point pour le gagnant lorsqu'il y a un gagnant
-				- 0 point en cas de match nul
-			*/
-        void initScores();
-            /* initialise à 0 les attributs scoreJoueur et scoreMachine
-               NON indispensable */
-        void initCoups();
-            /* initialise à rien les attributs coupJoueur et coupMachine
-               NON indispensable */
-
-
-         ///* Attributs du Modèle
-     private:
-        unsigned int scoreJoueur;   // score actuel du joueur
-        unsigned int scoreMachine;  // score actuel de la Machine
-        UnCoup coupJoueur;          // dernier coup joué par le joueur
-        UnCoup coupMachine;         // dernier coup joué par la machine
+private:
+    Ui::chifoumi *ui;
 };
-
 #endif // CHIFOUMI_H
